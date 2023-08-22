a00c9ebed597f46c105b385985acc0d88766ade9
Lucas Krämer de Sousa <lucas.kramer.sousa@gmail.com>
2023-08-22 20:15:02 -0300

    ✨ Add Property repository

---
+++ src/repositories/PropertyRepository.ts
@@ -0,0 +1,68 @@
+import { Property } from '../entities';
+import { FilterDTO, PropertyDTO } from '../dtos';
+import AppDataSource from '../dataSource';
+
+export const PropertyRepository = AppDataSource.getRepository(Property).extend({
+  async search(filters: FilterDTO): Promise<PropertyDTO[]> {
+    const queryBuilder = this.createQueryBuilder('property').select([
+      'property.id as id',
+      'property.address as address',
+      'property.bedrooms as bedrooms',
+      'property.bathrooms as bathrooms',
+      'property.price as price',
+      'property.type as type',
+    ]);
+
+    if (filters.address) {
+      queryBuilder.andWhere('property.address LIKE :address', {
+        address: `%${filters.address}%`,
+      });
+    }
+
+    if (filters.minBedrooms) {
+      queryBuilder.andWhere('property.bedrooms >= :minBedrooms', {
+        minBedrooms: filters.minBedrooms,
+      });
+    }
+
+    if (filters.maxBedrooms) {
+      queryBuilder.andWhere('property.bedrooms <= :maxBedrooms', {
+        maxBedrooms: filters.maxBedrooms,
+      });
+    }
+
+    if (filters.minBathrooms) {
+      queryBuilder.andWhere('property.bathrooms >= :minBathrooms', {
+        minBathrooms: filters.minBathrooms,
+      });
+    }
+
+    if (filters.maxBathrooms) {
+      queryBuilder.andWhere('property.bathrooms <= :maxBathrooms', {
+        maxBathrooms: filters.maxBathrooms,
+      });
+    }
+
+    if (filters.minPrice) {
+      queryBuilder.andWhere('property.price >= :minPrice', {
+        minPrice: filters.minPrice,
+      });
+    }
+
+    if (filters.maxPrice) {
+      queryBuilder.andWhere('property.price <= :maxPrice', {
+        maxPrice: filters.maxPrice,
+      });
+    }
+
+    if (filters.type) {
+      queryBuilder.andWhere('property.type = :type', {
+        type: filters.type,
+      });
+    }
+
+    queryBuilder.skip(filters.offset).take(filters.limit);
+
+    return queryBuilder.execute();
+  },
+});
